e3865bafca6c5b2e5bcfb35dc8cd7cebf8d2c361
Bryan J Swift <bryan.j.swift@gmail.com>
2008-08-18 00:06:11 +0000

    initial commit of portfolio files

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> <!-- ends in footer.php -->
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Bryan J Swift's Portfolio</title>
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="portfolio.css" type="text/css" media="screen" />
+	</head>
+	<?php flush(); ?>
+	<body> <!-- ends in footer.php -->
+		<div id="header" class="wrap">
+			<h1 class="title">Works of Bryan J Swift</h1>
+			<ul id="nav">
+				<li class="parent hovered">Motorola</li>
+			</ul>
+		</div>
+		<div id="body" class="wrap"> <!-- ends in footer.php -->
+			<!-- content filled in here -->
+			<div class="project">
+				<h2 class="title fwnormal">Motorola Product Pages</h2>
+				<img src="img/Motorola-MobilePhones.png" alt="Motorola Mobile Phones Page" width="304" height="283" class="proj-img alignright" />
+				<p class="first">Through my work at the New York office of AKQA I had the privelege to work on the recently launched site redesign for Motorola. A fairly large portion of the work for the office was done in flash and so I was not involved. However, the <a href="http://www.motorola.com/consumers/v/index.jsp?vgnextoid=8b871df4f3d89110VgnVCM1000008406b00aRCRD&appInstanceName=default" target="_motorola" title="Motorola USA - Mobile Phones">phones browse</a> and <a href="http://www.motorola.com/consumers/v/index.jsp?vgnextoid=d44832a609e89110VgnVCM1000008406b00aRCRD&appInstanceName=default" target="_motorola" title="Motorola USA - Accessories">accessories browse</a> pages were to be completed using HTML, CSS, and JavaScript. The designs provided by our creative department were as usual top notch and provided an excellent look and feel.</p>
+				<p>With the design in hand I went to the business of creating the JavaScript classes, components, functions and methods which would eventually control nearly every aspect of the user interaction with these pages. Through the course of the JavaScript development there were a number of re-usable pieces of code which were eventually abstracted out in order to facilitate the development of future pages which would utilize some but not all of the page's features.</p>
+			</div>
+			<!-- end content -->
+		</div> <!-- end of div.body -->
+	</body>
+</html>
