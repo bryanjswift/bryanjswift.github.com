4a479b02036226e3040b8a3097a930d42ccbb980
Bryan J Swift <bryan.j.swift@gmail.com>
2009-04-29 05:19:25 +0000

    updated some copy which wasn't correct

--- includes/smirnoff-gateway.php
+++ includes/smirnoff-gateway.php
@@ -3,5 +3,5 @@
 	<img src="img/Smirnoff-Gateway.png" alt="Smirnoff Age Gate" width="304" height="199" class="proj-img alignright" />
 	<p class="first">The team at AKQA's New York office were once again instrumental in enabling me to complete the work on the Smirnoff Gateway. There are a number of custom JavaScript components at work in this page the most notable of which is the keyboard navigation friendly select replacement for the country field. This field was the target of a great deal of criticism in the gateway which is currently in live on the <a href="http://smirnoff.com" title="Smirnoff">global Smirnoff brand site</a> because of how overly sensitive it is to the mouse wheel scrolling and how difficult it is to navigate with the keyboard.</p>
 	<p>The new gateway is MooTools (1.11) powered with several custom developed classes used to create stylized form elements. There are tentative plans to package and release the stylized form classes for public consumption in the future.</p>
-	<p>A mockup of the gateway (not hooked up to the live server) is <a href="http://smirnoff.com" title="Smirnoff Gateway Redesign">viewable live before entering smirnoff.com</a>.</p>
+	<p>The gateway is <a href="http://smirnoff.com" title="Smirnoff Gateway Redesign">viewable live before entering smirnoff.com</a>.</p>
 </div>
