8ddcd67b591bfab19ed7f3071428dbe2f8264977
Bryan J Swift <bryan.j.swift@gmail.com>
2009-05-19 18:10:42 +0000

    adding information about works

--- index.php
+++ index.php
@@ -3,10 +3,34 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<title>Bryan J Swift's Portfolio</title>
-		<link rel="stylesheet" href="portfolio.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="css/portfolio.css" type="text/css" media="screen" />
 	</head>
 	<?php flush(); ?>
 	<body>
-		
+		<h1>Selected works of Bryan J Swift</h1>
+		<p>The examples of client work I am showing were completed during my time at <a href="http://akqa.com">AKQA</a>. On these projects I served as the lead front end developer but the designs were produced by the talented members of the AKQA creative department.</p>
+		<h2>Client Work</h2>
+		<ol>
+			<li>
+				<h3>Motorola</h3>
+				<p>AKQA was commissioned to help Motorola rebrand, rearchitect, redesign and rebuild their global website and with the recently launched redesign my front end work is featured prominently on many of the templates.</p>
+			</li>
+			<li>
+				<h3>Smirnoff Gateway</h3>
+			</li>
+			<li>
+				<h3>Benjamin Moore Extranet</h3>
+			</li>
+		</ol>
+		<h2>Open Source Work</h2>
+		<ol>
+			<li>
+				<h3>MooTools Plugins</h3>
+			</li>
+			<li>
+				<h3>Vimblog Fork</h3>
+			</li>
+		</ol>
+		<script type="text/javascript" src="js/main.js"></script>
 	</body>
 </html>
