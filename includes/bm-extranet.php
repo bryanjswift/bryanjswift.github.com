12778a1bc4cbceda15e2baf6b95f7c232e476c0e
Bryan J Swift <bryan.j.swift@gmail.com>
2009-05-23 19:57:24 +0000

    removed the includes directory as the information is now included in the index

--- includes/bm-extranet.php
+++
@@ -1,7 +0,0 @@
-<div class="project">
-	<h2 class="title fwnormal">Benjamin Moore Extranet Project</h2>
-	<img src="img/Benjamin-Moore-Extranet.png" alt="Benjamin Moore Extranet Homepage" width="304" height="267" class="proj-img alignright" />
-	<p class="first">This project's primary goal was to streamline the process in which retailers (paint selling stores) interact with their supplier (Benjamin Moore). The goal was to provide a central place for retailers to access nearly all the information relevant to their account. AKQA's plan for meeting this requirement consisted of a number of sliding panels which contain various sections or panels of content.</p>
-	<p> User interaction on this page is fairly high in order to access all the information available, for example clicking on a tab slides the central panel over to cover the tab and reveals the related content. The JavaScript of the page is powered almost entirely by jQuery (1.2.6) and a few select plugins. Once integrated with the CMS and authentication there will be a number of different displays for various user roles. The displays will largely consist of a varying number of tabs in the top-most content panels.</p>
-	<p>The unintegrated mockup for this project <a href="http://bryanjswift.com/extranet/index.html" title="Benjamin Moore Extranet mockup" rel="nofollow">is available for viewing</a>.</p>
-</div>
--- includes/smirnoff-extranet.php
+++
@@ -1,7 +0,0 @@
-<div class="project">
-	<h2 class="title fwnormal">Smirnoff Experience Extranet</h2>
-	<img src="img/Experience-Extranet.png" alt="Smirnoff Experience Extranet Upload Page" width="304" height="274" class="proj-img alignright" />
-	<p class="first">Once again through the business won by the team at the New York office of AKQA I worked on a project which was to serve as an internal resource for the marketing managers of Diageo for the Smirnoff brand. The development of this project was completed by myself and one of my colleagues in the Technology Department. For the majority of this project I served as the back-end developer customizing the version of the <a href="http://www.magnolia.info/home.html" title="Magnolia Content Management System">Magnolia CMS</a> we chose to build on top of by creating servlets for Ajax requests to interact with in order to streamline the content creation and display process.</p>
-	<p>However, as the project experienced scope creep and a deadline drawing ever closer I was able to bring my front end development skills to bear in order to create the upload page pictured. The content nodes this upload page populated had only two required fields, title and description, the rest were optional but the information was sparse without it. The interesting thing about this form is it provided the ability to add any number of description blocks or gallery images.</p>
-	<p>This site is unfortunately not available for viewing.</p>
-</div>
