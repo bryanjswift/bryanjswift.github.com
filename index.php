13512acdc81f9333741165a079ba9de40b7822cd
Bryan J Swift <bryan.j.swift@gmail.com>
2009-05-23 17:41:41 +0000

    updated text for Motorola and Smirnoff Gateway projects

--- index.php
+++ index.php
@@ -14,11 +14,13 @@
 			<li>
 				<h3>Motorola</h3>
 				<p>AKQA was commissioned to help Motorola rebrand, rearchitect, redesign and rebuild their global website and with the recently launched redesign my front end work is featured prominently on many of the templates/pages.</p>
-				<p>I created most of the JavaScript classes and components which control many of the interactions between the user and the site and these elements were created with a strong object oriented approach in order to facilitate code re-use on subsequent endeavors with Motorola.</p>
+				<p>I created most of the JavaScript classes and components which control the interactions between the user and the site. These elements were created in JavaScript utilizing an object oriented approach in order to facilitate code re-use on subsequent endeavors with Motorola. This project was interesting and exciting in a lot of ways because it pushed me to create lots of flexible UI components many of which replaced standard browser controls without compromising keyboard accessibility.</p>
+				<p>Some of the pages I am most pleased with are the <a href="http://www.motorola.com/consumers/v/index.jsp?vgnextoid=8b871df4f3d89110VgnVCM1000008406b00aRCRD&appInstanceName=default" title="Motorola Phones Browse Page">phones</a>, <a href="http://www.motorola.com/consumers/v/index.jsp?vgnextoid=d44832a609e89110VgnVCM1000008406b00aRCRD&appInstanceName=default" title="Motorola Accessories Browse Page">accessories</a> and the <a href="http://www.motorola.com/Consumers/US-EN/Consumer-Product-and-Services/ch.Mobile-Phones.compare?pMotProductsToCompare=52179_57471_40906&productId=52179&productId=57471&productId=40906" title="Motorola Aura, Motorola Evoke, Motorola Rokr comparison page">product comparison page</a>. As with anything finished work there are some things I would like to change or tweak but all things considered I believe these turned out well.</p>
 			</li>
 			<li>
 				<h3>Smirnoff Gateway</h3>
-				<p>Diageo was having trouble with an abnormally high dropoff rate at the age validation step prior to gaining access to the site.</p>
+				<p>Diageo was having trouble with an abnormally high dropoff rate at the age validation step prior to gaining access to the site and felt the dropoff was related to the Gateway being in Flash. So they asked AKQA to redesign and rebuild it for them.</p>
+				<p>Once the creative department had finished creating an updated look in psd format it was handed off to me for top to bottom implementation. One of the more interesting challenges of this was backporting a number of MooTools plugins (which I had previously written) for use with a legacy version which broke compatibility in a bunch of ways.</p>
 			</li>
 			<li>
 				<h3>Benjamin Moore Extranet</h3>
