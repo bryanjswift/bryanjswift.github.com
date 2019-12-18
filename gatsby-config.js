a380f72cda9775aede173806757eef373887f9a9
Bryan Swift <mail@bryanwrit.es>
2019-12-18 11:10:49 -0600

    Add config for `gatsby-source-filesystem` and `gatsby-transformer-remark`

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,28 @@
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/_posts`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
   ],
 }
